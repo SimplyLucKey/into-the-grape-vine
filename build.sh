e7cbf0f3e03d847defd68b295d2bd734df38b85d
William <will.c.playground@gmail.com>
2026-06-27 00:23:40 -0700

    cleaner build shell script

--- build.sh
+++ build.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-# Copies shared files into each browser folder.
+# Copies all shared files into each browser folder.
 # Run this after any change to shared/ before loading the extension.
 
-cp shared/content.js chrome/
-cp shared/background.js chrome/
-cp shared/popup.html chrome/
-cp shared/popup.js chrome/
-
-cp shared/content.js firefox/
-cp shared/background.js firefox/
-cp shared/popup.html firefox/
-cp shared/popup.js firefox/
+cp shared/*.{js,html} chrome/
+cp shared/*.{js,html} firefox/
 
 echo "Build complete."
